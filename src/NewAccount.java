60e65c4ea7c4fde746a268ed5a7d489179771ffb
Gourav Khanna <gourav.khanna@profinch.com>
2020-08-13 10:07:36 +0530

    Bank Transaction Assignment

---
+++ src/NewAccount.java
@@ -0,0 +1,28 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class NewAccount {
+    public static String name;
+    public static int accNo=523456;
+    public  static String PAN, acctype,gender,emailID,Branch;
+    public  static String mobileNo;
+    Scanner sc=new Scanner(System.in);
+
+
+    public static String getDate() {
+        DateFormat df = new SimpleDateFormat();
+        Date dateobj = new Date();
+        return df.format(dateobj);
+    }
+
+    public static void CustomerDetail() {
+        System.out.println("Hi," + name + " you have successfully added account number" +accNo+" with " +acctype + " account with PAN number:" + PAN + " and emailID: " + emailID + "& Mobile No.:" + mobileNo+" has been registered with our bank on"+getDate());
+
+    }
+
+
+
+}
+
