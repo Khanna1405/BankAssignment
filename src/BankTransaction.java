ad364a9663c76a2f88a31015d5ae9fbdfc14d87f
Gourav Khanna <gourav.khanna@profinch.com>
2020-08-14 11:54:51 +0530

    update

--- src/BankTransaction.java
+++ src/BankTransaction.java
@@ -8,21 +8,25 @@ public class BankTransaction extends Account {
 
 
      static void Transaction() {
-        System.out.println("Please choose Transaction option: enter 1 for debit and 2 for Credit:");
+        System.out.println("Please choose Transaction option: enter 1 for Credit and 2 for debit:");
         Scanner scanner = new Scanner(System.in);
         UserInput = scanner.nextInt();
         switch (UserInput) {
             case 1:
-                System.out.println("Please Enter the amount to be debited:");
-                debit = scanner.nextInt();
-                System.out.println("User having registered mobile no:" + mobileNo + " debited " + debit + "rupees from the account no. " + accNo);
-                break;
-            case 2:
                 System.out.println("Please Enter Credit Amount:");
                 Credit = scanner.nextInt();
                 System.out.println("User having registered mobile no." + mobileNo + " credited " + Credit + " rupees to the account no: " + accNo);
 
                 break;
+                case 2:
+                    System.out.println("Please Enter the amount to be debited:");
+                    debit = scanner.nextInt();
+                    if (debit>Credit)
+                        System.out.println("You can't debit that much amount");
+                    else
+                System.out.println("User having registered mobile no:" + mobileNo + " debited " + debit + "rupees from the account no. " + accNo);
+                break;
+
             default:
                 System.out.println("Not a valid option:");
         }
@@ -30,24 +34,6 @@ public class BankTransaction extends Account {
 
     }
 
-/*
-    void BankStatement() {
-        System.out.println("Please enter 1 for debited amount statement and 2 for credited amt statement ");
-        Scanner sc = new Scanner(System.in);
-        UserInput = sc.nextInt();
-        switch (UserInput) {
-            case 1:
-                System.out.println(name + " have " + accNo + "account no." + " having " + acctype + " account type " + "debited " + debit + "rupees on " + getDate());
-                break;
-            case 2:
-                System.out.println(name + " have " + accNo + "account no." + " having " + acctype + " account type " + "Credited " + Credit + "rupees on " + getDate());
-                break;
-            default:
-                System.out.println("Not a valid option");
-        }
 
-    }
-
- */
 }
 
